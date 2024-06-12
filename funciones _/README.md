f65e18d80b66d2eccf9128d7bf3a4b08c32bf822
ronny102829 <ronnyromanastoyauri@gmail.com>
2024-06-12 10:25:08 -0500

    trabajo 12-06-2025

--- funciones _/README.md
+++ funciones _/README.md
@@ -38,4 +38,67 @@ def saludo():
 *el archivo `lectur.py`
 ## retornandop multiples valores
 el secretyo es aserlo mediante un tipo de dato estructurado 
+`
+
+
+## parametros y argumentodos
+si uan funciona  no dispusiera de valores b de entrada estaria limitadea en su actuacion. es por ello que los`parameteos` no permiten variar losd datos que consume una funcion
+para obtener distintos resultados
+**ejemplo**
+
+*crear una funcion que resive un valor numerico y retoorna su raiz cuadrada*
 ```python
+def sqrt(valor):
+    return valor**(1/2)
+# NOTA: en este cso, el valor 4 es un argumento de la funcion
+sqrt(4)
+```
+cuandop llamamos  a un funcion con ` argumentos`, los valores de estos argumentos  se copian en los correpondientes `parametros`,dentro de las funcion.
+```python
+def ejem(a,b,c):
+    return a+b+c
+ejem(4,5,6)
+```
+### argumentos nominales 
+en esta aproximamcion  los argumentosno sosn copiados en un orden espesifico sino que  **se asigna por nombre acada perimetro** . ello nos evita el problema de conocer o reconocer cual  es el orden  e los parametros  en la  funcion. para utilizarlo, basta con ralisar un asignacion de acada argumento en la propia llamada  de la funcion.
+
+**ejemplo** 
+```python
+def  build_cpu(familia,num_core,frecuencia):
+    print(f"""
+    la cpu es de la famila {familia},
+    con {num_core} cores y una 
+    frecuencia  de {frecuencia}
+    """)
+# haciendo uso  de argumentos dominales 
+build_cpu("intel",4,2,7)
+build_cpu(num_core=4,familia="intel", frecuencia=2.7)
+```
+### argumentos posicionales 
+los argumnetos son copias en un orden espesifico, en ste caos de bemos conocer o recordar cual es el orden de los parametros .
+
+**ejemplo**
+```python
+def  build_cpu(familia,num_core,frecuencia):
+    print(f"""
+        la cpu es de la famila {familia},
+        con {num_core} cores y una 
+        frecuencia  de {frecuencia}
+    """)
+#haciendo uso  de argumentos dominales 
+build_cpu("intel",4,2,7)
+```
+### parametrsiopor defecto
+es posible spisificar **valores por defecto** en losn parametros de una funcion , en el caso que no se proporcione un valor al argumento  en la llmada a la funcion, el paraetro cvorrespondiente  tomara el valor defenidom por defecto .
+
+**ejemplo**
+```python
+def alumnos(nom,app,estado="aprobado"):
+
+alumnos("ruth","castillo")
+alumnos("anthony","crucez","desaprovado")
+```
+
+###  desenpaquetado/ empequetado de argumentos(tarea)
+
+## funciones internas de python(tarea)
--- funciones _/lecture.py
+++ funciones _/lecture.py
@@ -1,10 +1,88 @@
-#return el valor que podre hacer uso
-#crear una funcion que retorne 10, muetre por termoinaal si es par 
-#siempre que el valor que tretorno mi funcion se utiliza en mas sen5tencias en massentyencias u operaciones 
-def diez():
-    return 10
-if diez()%2==0:
-    print("es par")
-else:
-    print(" es inpar")
-    #print solo muestar por terminal 
+# #return el valor que podre hacer uso
+# #crear una funcion que retorne 10, muetre por termoinaal si es par 
+# #siempre que el valor que tretorno mi funcion se utiliza en mas sen5tencias en massentyencias u operaciones. 
+# def diez():
+#     return 10
+# if diez()%2==0:
+#     print("es par")
+# else:
+#     print(" es inpar")
+#     #print solo muestar por terminal 
+# #return cuando queremos  que nuestra funciones devuelbe  o retorne in tipo de dato o un tipo nde dato estructurado
+# #crear una funcion k me muestre el producto de dos numeros. 
+# def producto(a,b):
+#     return a*b
+# print(producto(5,4))
+# # crear una funcion queretorne una lista de tres numeros.
+# def lista_numeros():
+#     return[3,2,6]
+# # crear una funcion k por pa5rametro resiva un nombre  y retorne un mensaje  de bienvenida  con el nombre.
+# def mensaje(nombre):
+#     print(f"hola,{nombre}, bienvinido al chongo")
+# mensaje("jose")
+  
+#crear una FUNCION  QUE RECIVA POR parametro una lista  de numeros y me devuelva el numero menor , mostar por terminal el valor retornado  por la funcion
+
+#-ejemplo clace 
+# lista=[1,2,3,4]
+# def Min(l):
+#     minimo=1[0]
+#     for n in l:
+#         if n< minimo:
+#             minimo=n
+#     return minimo
+# print(min(lista))
+## trabajo yo
+       
+# def encontrar_numero_menor(lista_numeros):
+#     if len(lista_numeros) == 0:
+#         return None
+#     else:
+#         return min(lista_numeros)
+
+# # Ejemplo de uso
+# numeros = [10, 5, 8, 3, 12]
+# numero_menor = encontrar_numero_menor(numeros)
+# print("El número menor de la lista es:", numero_menor)
+
+
+# def encontrar_numero_menor(lista_numeros):
+#  if len(lista_numeros) == 0:       print("La lista está vacía. No se puede encontrar el número menor.")
+#  else:
+#         numero_menor = min(lista_numeros)
+#         print("El número menor de la lista es:", numero_menor)
+#         return numero_menor
+
+# # Ejemplo de uso
+# numeros = [10, 5, 8, 3, 12]
+# numero_menor = encontrar_numero_menor(numeros)
+# crear una funcion que reciba como parametro el nomnre y la edad  de una persona mi funcion debera  retornar un diccionario  con los datos,
+# luego mostrar por terminal rl valor de retorno de mi funcion
+
+# Ejemplo de uso
+nombre = "Juan"
+edad = 30
+def persona(nombre, edad):
+    datos_persona = {
+        "nombre": nombre,
+        "edad": edad
+    }
+    return datos_persona
+
+datos_persona =persona(nombre, edad)
+print("los datos de la persona es:", datos_persona)
+
+
+nombre =input("ingrese su nombre:")
+edad =int(imput("ingrese su edad"))
+def persona(nombre, edad):
+    # datos_persona = {
+    #     "nombre": nombre,
+    #     "edad": edad
+    #}
+    return disct(
+        nombre=nombre,
+        edad=edad
+    )
+
+print(persona(nombre, edad))
